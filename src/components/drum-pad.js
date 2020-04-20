0df5483cac2cf88070fbc0bd8bea0cc4b4bcbf63
Erich Keil <39918654+zenlex@users.noreply.github.com>
2020-04-19 22:26:19 -0400

    added fcc test suite and starting comments

---
+++ src/components/drum-pad.js
@@ -0,0 +1,3 @@
+//drum pad element
+//must be clickable and also must be assignable to a hotkey
+//must be able to assign audio element with id ="{hotkey}"
---
+++ src/containers/drum-machine.js
@@ -0,0 +1,4 @@
+//container for all other elements
+//must have id #drum machine
+//must contain display element with id="display" (make a div with id display and put display react component inside)
+//9 drum pads in q,w,e,a,s,d,z,x,c order
