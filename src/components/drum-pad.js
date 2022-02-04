ff9187a0b34b8a96e3810e4c778d7edfbb713b1e
zenlex <erichrkeil@gmail.com>
2022-02-03 21:37:53 -0600

    CSS Display resize fix

--- src/components/drum-pad.js
+++ src/components/drum-pad.js
@@ -5,80 +5,73 @@
 import React from 'react';
 
 class DrumPad extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            padStyle: 'drum-pad pad-inactive'
-        }
-        this.playSound = this.playSound.bind(this);
-        this.togglePadStyle = this.togglePadStyle.bind(this);
-        this.clickHandle = this.clickHandle.bind(this);
-        this.keyPressHandle = this.keyPressHandle.bind(this);
+    this.state = {
+      padStyle: 'drum-pad pad-inactive'
     }
+    this.playSound = this.playSound.bind(this);
+    this.togglePadStyle = this.togglePadStyle.bind(this);
+    this.clickHandle = this.clickHandle.bind(this);
+    this.keyPressHandle = this.keyPressHandle.bind(this);
+  }
 
-componentDidMount(){
-   //create callback to check if the key was the hotkey for the pad and trigger the audio. Should also change a state property to activate the button css animation
+  componentDidMount() {
     document.addEventListener('keydown', this.keyPressHandle);
-}
+  }
 
-componentWillUnmount(){
+  componentWillUnmount() {
     document.removeEventListener('keydown', this.keyPressHandle);
-}
+  }
 
-
-playSound(){
+  playSound() {
     this.togglePadStyle();
     const clip = document.getElementById(this.props.hotkey);
-    clip.currentTime=0;
+    clip.currentTime = 0;
     var playPromise = clip.play();
-
-      if (playPromise !== undefined) {
-        playPromise
-          .then(_ => {
-            // Automatic playback started!
-            // Show playing UI.
-            console.log("audio played auto");
-          })
-          .catch(error => {
-            // Auto-play was prevented
-            // Show paused UI.
-            console.log("playback prevented");
-          });
-      }
+    if (playPromise !== undefined) {
+      playPromise
+        .then(_ => {
+          console.log("audio played auto");
+        })
+        .catch(error => {
+          console.log("playback prevented");
+        });
+    }
     this.props.updateDisplay(this.props.clipId);
-    setTimeout(()=>this.togglePadStyle(), 150);
-}
+    setTimeout(() => this.togglePadStyle(), 150);
+  }
 
-clickHandle(){
+  clickHandle() {
     this.playSound();
-}
+  }
 
-keyPressHandle(e){
- 
-    if(e.key === this.props.hotkey || e.keyCode === this.props.keyCode){
-        this.playSound();
+  keyPressHandle(e) {
+
+    if (e.key === this.props.hotkey || e.keyCode === this.props.keyCode) {
+      this.playSound();
     }
-   
-}
 
-togglePadStyle(){
-    if(this.state.padStyle === 'drum-pad pad-inactive'){
-        this.setState({padStyle: 'drum-pad pad-active'})
+  }
+
+  togglePadStyle() {
+    if (this.state.padStyle === 'drum-pad pad-inactive') {
+      this.setState({ padStyle: 'drum-pad pad-active' })
     } else {
-        this.setState({padStyle: 'drum-pad pad-inactive'})
+      this.setState({ padStyle: 'drum-pad pad-inactive' })
     }
-}
+  }
 
-    render(){
-        return(
-            <div className = {this.state.padStyle} id={this.props.clipId}
-                onClick = {this.clickHandle}>
-                <audio className = 'clip' id={this.props.hotkey} src={this.props.audioSrc}> </audio>
-                    {this.props.hotkey}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={this.state.padStyle} id={this.props.clipId}
+        onClick={this.clickHandle}>
+        <audio className='clip' id={this.props.hotkey} src={this.props.audioSrc}> </audio>
+        {this.props.hotkey}
+      </div>
+    )
+  }
 }
 
 export default DrumPad;
